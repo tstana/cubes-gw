0784fad5705cd99ea0994af6eb36d16c0a5ea084
Theodor Stana <stana@kth.se>
2017-02-01 15:37:00 +0100

    UART: Implemented TX and simulated

--- hdl/bemicro/uart.vhd
+++ hdl/bemicro/uart.vhd
@@ -37,24 +37,44 @@ end entity uart;
 architecture behav of uart is
   
   --===============================================================================================
+  -- Type declarations
+  --===============================================================================================
+  type t_state_tx is (
+    TX_IDLE,
+    TX_START,
+    TX_DATA,
+    TX_STOP
+  );
+
+  --===============================================================================================
   -- Signal declarations
   --===============================================================================================
+  signal baud_en            : std_logic;
   signal baud_div           : unsigned(g_baud_div_bits-1 downto 0);
   signal baud_tick          : std_logic;
   signal baud_div_halfbit   : unsigned(g_baud_div_bits-2 downto 0);
   signal baud_halfbit_tick  : std_logic;
   
-  signal tmp : unsigned(7 downto 0);
-  signal delay : std_logic;
+  signal state_tx           : t_state_tx;
+  signal tx_baud_en         : std_logic;
+  signal tx_sreg            : std_logic_vector(7 downto 0);
+  signal tx_data_count      : unsigned(2 downto 0);
   
+  signal rxd                : std_logic;
+  signal rxd_d0             : std_logic;
+  signal rx_baud_en         : std_logic;
+
 begin
   
-  --===========================================================================
+  --===============================================================================================
   -- Baud rate generator
-  --===========================================================================
+  --===============================================================================================
+  -- Baud divider enable
+  baud_en <= tx_baud_en or rx_baud_en;
+  
   -- Divider at half-bit
   baud_div_halfbit <= unsigned(baud_div_i(baud_div'high downto 1));
-  
+
   -- Baud rate generation process
   p_baud_div : process (clk_i, rst_n_a_i) is
   begin
@@ -63,40 +83,96 @@ begin
       baud_tick         <= '0';
       baud_halfbit_tick <= '0';
     elsif rising_edge(clk_i) then
-      baud_tick <= '0';
-      baud_div  <= baud_div + 1;
-      if (baud_div = unsigned(baud_div_i)) then
-        baud_div  <= (others => '0');
-        baud_tick <= '1';
-      end if;
+      if (baud_en = '1') then
+        baud_tick <= '0';
+        baud_div  <= baud_div + 1;
+        if (baud_div = unsigned(baud_div_i)) then
+          baud_div  <= (others => '0');
+          baud_tick <= '1';
+        end if;
 
-      baud_halfbit_tick <= '0';
-      if (baud_div(baud_div'high-1 downto 0) = baud_div_halfbit) then
-        baud_halfbit_tick <= '1';
+        baud_halfbit_tick <= '0';
+        if (baud_div(baud_div'high-1 downto 0) = baud_div_halfbit) then
+          baud_halfbit_tick <= '1';
+        end if;
       end if;
     end if;
   end process p_baud_div;
 
-  process (clk_i, rst_n_a_i) is
+  --===========================================================================
+  -- TX
+  --===========================================================================
+  p_tx : process (clk_i, rst_n_a_i) is
   begin
     if (rst_n_a_i = '0') then
-      tmp <= (others => '0');
-      delay <= '0';
+      state_tx      <= TX_IDLE;
+      txd_o         <= '1';
+      tx_ready_o    <= '1';
+      tx_baud_en    <= '0';
+      tx_sreg       <= (others => '0');
+      tx_data_count <= (others => '0');
     elsif rising_edge(clk_i) then
-      if (delay = '0') then
-        if baud_tick = '1' then
-          delay <= '1';
-        end if;
-      else
-        tmp <= tmp + 1;
-        if (tmp = 11) then 
-          tmp <= (others => '0');
-          delay <= '0';
-        end if;
-      end if;
-    end if;
-  end process;
 
-  txd_o <= delay;
+      case state_tx is
+
+        when TX_IDLE =>
+          txd_o         <= '1';
+          tx_ready_o    <= '1';
+          tx_data_count <= (others => '0');
+          tx_baud_en    <= '0';
+          if (tx_start_p_i = '1') then
+            tx_ready_o  <= '0';
+            tx_sreg     <= tx_data_i;
+            txd_o       <= '0';
+            tx_baud_en  <= '1';
+            state_tx    <= TX_START;
+          end if;
+          
+        when TX_START =>
+          if (baud_tick = '1') then
+            state_tx  <= TX_DATA;
+          end if;
+          
+        when TX_DATA =>
+          txd_o <= tx_sreg(0);
+          if (baud_tick = '1') then
+            tx_sreg       <= '0' & tx_sreg(7 downto 1);
+            tx_data_count <= tx_data_count + 1;
+            if (tx_data_count = ((tx_data_count'range) => '1')) then
+              txd_o    <= '1';
+              state_tx <= TX_STOP;
+            end if;
+          end if;
+          
+        when TX_STOP =>
+          if (baud_tick = '1') then
+            state_tx  <= TX_IDLE;
+          end if;
+          
+        when others =>
+          state_tx      <= TX_IDLE;
+          txd_o         <= '1';
+          tx_data_count <= (others => '0');
+          tx_baud_en    <= '0';
+          
+      end case;
+
+    end if;
+  end process p_tx;
   
+  --===========================================================================
+  -- RX
+  --===========================================================================
+  -- Synchronize RXD port to clk_i domain
+  cmp_sync_rx : gc_sync_ffs
+    port map
+    (
+      clk_i     => clk_i,
+      rst_n_i   => rst_n_a_i,
+      data_i    => rxd_i,
+      synced_o  => rxd
+    );
+    
+  rx_baud_en <= '0';
+
 end architecture behav;
--- sim/tb_uart.vhd
+++ sim/tb_uart.vhd
@@ -2,8 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+use work.genram_pkg.all;
+
 
 entity tb_uart is
+  generic (
+    g_baud_div_bits : natural := 10
+  );
 end entity;
 
 
@@ -13,6 +18,22 @@ architecture behav of tb_uart is
   -- Constant declarations
   --===============================================================================================
   constant c_clk_per : time := 10ns;
+  constant c_baud_div_int : natural := 867;
+  constant c_baud_div     : std_logic_vector := 
+                  std_logic_vector(to_unsigned(c_baud_div_int, f_log2_size(c_baud_div_int)));
+  
+  --===============================================================================================
+  -- Function declarations
+  --===============================================================================================
+  procedure sample(
+    signal clk : in  std_logic;
+    signal sig : out std_logic
+  ) is
+  begin
+    sig <= '1';
+    wait until rising_edge(clk);
+    sig <= '0';
+  end procedure;
   
   --===============================================================================================
   -- Component declarations
@@ -21,28 +42,32 @@ architecture behav of tb_uart is
   component uart is
     generic
     (
-    g_baud_div_bits : natural := 16
+      g_baud_div_bits : natural := 16
     );
     port
     (
       -- Clock, reset
-      clk_i       : in  std_logic;
-      rst_n_a_i   : in  std_logic;
-      
+      clk_i         : in  std_logic;
+      rst_n_a_i     : in  std_logic;
+
       -- Ports to external world
-      rxd_i       : in  std_logic;
-      txd_o       : out std_logic;
-      
+      rxd_i         : in  std_logic;
+      txd_o         : out std_logic;
+
       -- Ports to other logic
-      baud_div_i  : in  std_logic_vector(g_baud_div_bits-1 downto 0);
-      rx_data_o   : out std_logic_vector(7 downto 0);
-      tx_data_i   : in  std_logic_vector(7 downto 0);
-      
-      rx_ready_o  : out std_logic;
-      tx_ready_o  : out std_logic;
-      frame_err_o : out std_logic
+      baud_div_i    : in  std_logic_vector(g_baud_div_bits-1 downto 0);
+
+      tx_data_i     : in  std_logic_vector(7 downto 0);
+      tx_start_p_i  : in  std_logic;
+      tx_ready_o    : out std_logic;
+
+      rx_ready_o    : out std_logic;
+      rx_data_o     : out std_logic_vector(7 downto 0);
+
+      frame_err_o   : out std_logic
     );
   end component uart;
+
 
   --===============================================================================================
   -- Signal declarations
@@ -50,11 +75,34 @@ architecture behav of tb_uart is
   signal clk_100meg     : std_logic;
   signal rst_n          : std_logic;
   
-  signal txd_o          : std_logic;
+  signal txd, rxd       : std_logic;
 
+  signal tx_start       : std_logic;
+  signal tx_data        : unsigned(7 downto 0) := x"30";
+  signal tx_data_d0     : unsigned(7 downto 0) := x"30";
+  signal tx_ready       : std_logic;
+  signal start_delay    : unsigned(7 downto 0);
+  
+  signal rx_sreg        : std_logic_vector(7 downto 0) := (others => '0');
+  signal rx_count       : unsigned(2 downto 0) := (others => '0');
+  
+  signal baud_x8_count  : unsigned(g_baud_div_bits-4 downto 0);
+  signal baud_x8_div    : unsigned(g_baud_div_bits-4 downto 0);
+  signal baud_x8_tick   : std_logic;
+  signal baud_en        : std_logic;
+  signal baud_rx_en     : std_logic;
+  signal baud_tx_en     : std_logic;
+  signal baud_sreg      : std_logic_vector(7 downto 0);
+  signal baud_div       : unsigned(2 downto 0);
+  signal baud_tick      : std_logic;
+  
+  signal mon_sample     : std_logic := '0';
   
 begin
 
+  --===============================================================================================
+  -- Clock and reset
+  --===============================================================================================
   p_clk : process is
   begin
     clk_100meg <= '0';
@@ -70,30 +118,130 @@ begin
     rst_n <= '1';
     wait;
   end process p_rst;
+  
+  --===============================================================================================
+  -- Stimuli process
+  --===============================================================================================
+  p_stim : process (clk_100meg, rst_n) is
+  begin
+    if (rst_n = '0') then
+      tx_start    <= '0';
+      start_delay <= (others => '0');
+      tx_data     <= x"30";
+      tx_data_d0  <= x"30";
+    elsif rising_edge(clk_100meg) then
+      tx_start    <= '0';
+      tx_data_d0  <= tx_data;
+      if (tx_ready = '1') then
+        start_delay <= start_delay + 1;
+        if (start_delay = (start_delay'range => '1')) then
+          tx_data  <= tx_data + 1;
+          tx_start <= '1';
+          if (tx_data = x"39") then
+            tx_data <= x"13";
+          elsif (tx_data = x"13") then
+            tx_data <= x"30";
+          end if;
+        end if;
+      end if;
+    end if;
+  end process p_stim;
 
+  --===============================================================================================
+  -- Instantiate DUT
+  --===============================================================================================
   DUT : uart
     generic map
     (
-      g_baud_div_bits => 10
+      g_baud_div_bits => f_log2_size(c_baud_div_int)
     )
     port map
     (
       -- Clock, reset
-      clk_i       => clk_100meg,
-      rst_n_a_i   => rst_n,
+      clk_i         => clk_100meg,
+      rst_n_a_i     => rst_n,
 
       -- Ports to external world
-      rxd_i       => '0',
-      txd_o       => txd_o,
+      rxd_i         => '0',
+      txd_o         => txd,
 
       -- Ports to other logic
-      baud_div_i  => "1101100100",
-      rx_data_o   => open,
-      tx_data_i   => (others => '0'),
+      baud_div_i    => c_baud_div,
+      tx_start_p_i  => tx_start,
+      tx_data_i     => std_logic_vector(tx_data_d0),
+      tx_ready_o    => tx_ready,
 
-      rx_ready_o  => open,
-      tx_ready_o  => open,
-      frame_err_o => open
+      rx_data_o     => open,
+      rx_ready_o    => open,
+
+      frame_err_o   => open
     );
+
+  --===============================================================================================
+  -- DUT monitor process
+  --===============================================================================================
+  p_mon : process is
+  begin
+    wait until tx_start = '1';
+    baud_en <= '1';
+    wait until baud_div = to_unsigned(4, baud_div'length);
+    sample(clk_100meg, mon_sample);
+    assert (txd = '0') report "Start bit wrong!" severity error;
+    while rx_count /= (rx_count'range => '1') loop
+      wait until baud_div = to_unsigned(4, baud_div'length);
+      sample(clk_100meg, mon_sample);
+      rx_sreg <= txd & rx_sreg(7 downto 1);
+      rx_count <= rx_count + 1;
+    end loop;
+    wait until baud_div = to_unsigned(4, baud_div'length);
+    sample(clk_100meg, mon_sample);
+    assert (txd = '1') report "Stop bit wrong!" severity error;
+    baud_en <= '0';
+  end process;
+  
+  --===============================================================================================
+  -- Baud rate generator
+  --===============================================================================================  
+  -- Create x8 baud-rate clock by dividing input baud rate divider by eight
+  baud_x8_div <= unsigned(c_baud_div(g_baud_div_bits-1 downto 3));
+  
+  -- Divide clk_i to baud rate x8
+  p_baud_x8_div : process (clk_100meg, rst_n) is
+  begin
+    if (rst_n = '0') then
+      baud_x8_count <= to_unsigned(1, g_baud_div_bits-3);
+      baud_x8_tick  <= '0';
+    elsif rising_edge(clk_100meg) then
+      baud_x8_count <= (others => '0');
+      if (baud_en = '1') then
+        baud_x8_count <= baud_x8_count - 1;
+        baud_x8_tick  <= '0';
+        if (baud_x8_count = 1) then
+          baud_x8_count <= baud_x8_div;
+          baud_x8_tick  <= '1';
+        end if;
+      end if;
+    end if;
+  end process p_baud_x8_div;
+  
+  -- Divide baud x8 clock
+  p_baud_div : process (clk_100meg, rst_n) is
+  begin
+    if (rst_n = '0') then
+      baud_div  <= (others => '0');
+      baud_tick <= '0';
+    elsif rising_edge(clk_100meg) then
+      baud_div <= (others => '0');
+      if (baud_en = '1') then
+        baud_tick <= '0';
+        if (baud_x8_tick = '1') then
+          baud_div <= baud_div - 1;
+          if (baud_div = (baud_div'range => '0')) then
+            baud_tick <= '1';
+          end if;
+        end if;
+      end if;
+    end if;
+  end process p_baud_div;
 
 end architecture;
