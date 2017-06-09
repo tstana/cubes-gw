929dbb05f8a31c9686b325ba6073f1598f48069c
Theodor Stana <stana@kth.se>
2017-06-09 11:18:04 +0200

    wb_siphra_ctrl: No longer resetting CHANxxRATER on read
    This is not necessary, as the register would anyway be cleared on the next
    1-second tick, should no more events arrive.

--- hdl/modules/siphra_ctrl/wb_siphra_ctrl.vhd
+++ hdl/modules/siphra_ctrl/wb_siphra_ctrl.vhd
@@ -366,9 +366,7 @@ gen_chanrater : for i in 0 to 15 generate
     if (rst_n_a_i = '0') then
       chanrater(i) <= (others => '0');
     elsif rising_edge(clk_i) then
-      if (chanrater_read_p(i) = '1') then
-        chanrater(i) <= (others => '0');
-      elsif (tick_1sec_p = '1') then
+      if (tick_1sec_p = '1') then
         chanrater(i) <= x"0000" & std_logic_vector(chan_rate_counter(i));
       end if;
     end if;
